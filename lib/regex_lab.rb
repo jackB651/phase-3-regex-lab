b08f30402657882a59f1ae1bc7321234d45102cd
Jonathon <jwbialas@gmail.com>
2022-08-30 22:04:52 -0500

    commit tree 5547a7ad0319464893eab23ef92fe93be718affc

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+    word.match(/\W[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    # test.grep(/\b\w{1,5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
